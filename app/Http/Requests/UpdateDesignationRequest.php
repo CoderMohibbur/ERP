181db11f62ab891d69f19d1449c6acc4faa1ee7a
Mohibbur Rahman <mohibbur1994@gmail.com>
2025-05-14 08:04:10 +0600

    controller update

--- app/Http/Requests/UpdateDesignationRequest.php
+++ app/Http/Requests/UpdateDesignationRequest.php
@@ -3,26 +3,26 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateDesignationRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+    
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
+
     public function rules(): array
     {
         return [
-            //
+            'name' => [
+                'required',
+                'string',
+                'max:100',
+                Rule::unique('designations', 'name')->ignore($this->designation),
+            ],
         ];
     }
 }
