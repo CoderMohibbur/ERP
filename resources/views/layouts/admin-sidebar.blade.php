08f4a8fb0babe71c7407870509204c58afc9ad7a
user <mdtufsirulmiraj@gmail.com>
2025-05-14 07:06:35 +0600

    update

--- resources/views/layouts/admin-sidebar.blade.php
+++ resources/views/layouts/admin-sidebar.blade.php
@@ -21,8 +21,8 @@
                     class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-gray-200 dark:hover:bg-gray-700"
                     aria-controls="user-dropdown" data-collapse-toggle="user-dropdown" aria-expanded="false">
                     <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white"
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
-                        viewBox="0 0 24 24">
+                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                        fill="currentColor" viewBox="0 0 24 24">
                         <path fill-rule="evenodd"
                             d="M17 10v1.126c.367.095.714.24 1.032.428l.796-.797 1.415 1.415-.797.796c.188.318.333.665.428 1.032H21v2h-1.126c-.095.367-.24.714-.428 1.032l.797.796-1.415 1.415-.796-.797a3.979 3.979 0 0 1-1.032.428V20h-2v-1.126a3.977 3.977 0 0 1-1.032-.428l-.796.797-1.415-1.415.797-.796A3.975 3.975 0 0 1 12.126 16H11v-2h1.126c.095-.367.24-.714.428-1.032l-.797-.796 1.415-1.415.796.797A3.977 3.977 0 0 1 15 11.126V10h2Zm.406 3.578.016.016c.354.358.574.85.578 1.392v.028a2 2 0 0 1-3.409 1.406l-.01-.012a2 2 0 0 1 2.826-2.83ZM5 8a4 4 0 1 1 7.938.703 7.029 7.029 0 0 0-3.235 3.235A4 4 0 0 1 5 8Zm4.29 5H7a4 4 0 0 0-4 4v1a2 2 0 0 0 2 2h6.101A6.979 6.979 0 0 1 9 15c0-.695.101-1.366.29-2Z"
                             clip-rule="evenodd" />
@@ -48,16 +48,97 @@
                 </ul>
             </li>
 
+            {{-- Employee Management --}}
+            <li>
+                <a href="{{ route('admin.employmanagement.index') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                        class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-300 group-hover:text-green-600 dark:text-gray-400 dark:group-hover:text-white">
+                        <path fill-rule="evenodd"
+                            d="M8.25 6.75a3.75 3.75 0 1 1 7.5 0 3.75 3.75 0 0 1-7.5 0ZM15.75 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM2.25 9.75a3 3 0 1 1 6 0 3 3 0 0 1-6 0ZM6.31 15.117A6.745 6.745 0 0 1 12 12a6.745 6.745 0 0 1 6.709 7.498.75.75 0 0 1-.372.568A12.696 12.696 0 0 1 12 21.75c-2.305 0-4.47-.612-6.337-1.684a.75.75 0 0 1-.372-.568 6.787 6.787 0 0 1 1.019-4.38Z"
+                            clip-rule="evenodd" />
+                        <path
+                            d="M5.082 14.254a8.287 8.287 0 0 0-1.308 5.135 9.687 9.687 0 0 1-1.764-.44l-.115-.04a.563.563 0 0 1-.373-.487l-.01-.121a3.75 3.75 0 0 1 3.57-4.047ZM20.226 19.389a8.287 8.287 0 0 0-1.308-5.135 3.75 3.75 0 0 1 3.57 4.047l-.01.121a.563.563 0 0 1-.373.486l-.115.04c-.567.2-1.156.349-1.764.441Z" />
+                    </svg>
+
+                    <span class="menu-text hidden flex-1 ms-3 whitespace-nowrap">Employe Management</span>
+                </a>
+            </li>
+
+
+            {{-- Attendance System --}}
+            <li>
+                <a href="{{ route('admin.attendancesystem.index') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-300 group-hover:text-green-600 dark:text-gray-400 dark:group-hover:text-white"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                            d="M12 2a10 10 0 1 0 0 20 10 10 0 0 0 0-20Zm.75 5.25a.75.75 0 0 0-1.5 0v5c0 .414.336.75.75.75h3a.75.75 0 0 0 0-1.5h-2.25V7.25Z" />
+                    </svg>
+
+                    <span class="menu-text hidden flex-1 ms-3 whitespace-nowrap">Attendance System</span>
+                </a>
+            </li>
+
+
+            {{-- Project Management --}}
+            <li>
+                <a href="{{ route('admin.projectmanagement.index') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-300 group-hover:text-purple-600 dark:text-gray-400 dark:group-hover:text-white"
+                        fill="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path
+                            d="M3 4a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v16a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4zm5 3a1 1 0 0 0-1 1v9a1 1 0 0 0 2 0V8a1 1 0 0 0-1-1zm5 4a1 1 0 0 0-1 1v5a1 1 0 0 0 2 0v-5a1 1 0 0 0-1-1zm4-6a1 1 0 0 0-1 1v11a1 1 0 0 0 2 0V6a1 1 0 0 0-1-1z" />
+                    </svg>
+
+                    <span class="menu-text hidden flex-1 ms-3 whitespace-nowrap">Project Management</span>
+                </a>
+            </li>
+
+
+
+
+            {{-- Client Management --}}
+            <li>
+                <a href="{{ route('admin.clientmanagement.index') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-300 group-hover:text-orange-600 dark:text-gray-400 dark:group-hover:text-white"
+                        xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+                        <path
+                            d="M12 12c2.7 0 4.9-2.2 4.9-4.9S14.7 2.2 12 2.2 7.1 4.4 7.1 7.1 9.3 12 12 12zm0 2.2c-3.1 0-9.3 1.6-9.3 4.8V21h18.6v-2c0-3.2-6.2-4.8-9.3-4.8z" />
+                    </svg>
+
+                    <span class="menu-text hidden flex-1 ms-3 whitespace-nowrap">Client Management</span>
+                </a>
+            </li>
+
+
+
+            {{-- Billing & Invoicing --}}
+            <li>
+                <a href="{{ route('admin.billinginvoicing.index') }}"
+                    class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                    <svg class="flex-shrink-0 w-6 h-6 text-gray-500 group-hover:text-amber-600 dark:text-gray-400 dark:group-hover:text-white transition duration-300"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
+                        <path
+                            d="M4 2a1 1 0 0 0-1 1v18l3-2 3 2 3-2 3 2 3-2 3 2V3a1 1 0 0 0-1-1H4zm8 5a1 1 0 0 1 1 1v.25c1.24.29 2.25 1.38 2.25 2.75s-1.01 2.46-2.25 2.75V15a1 1 0 1 1-2 0v-1.25c-1.24-.29-2.25-1.38-2.25-2.75s1.01-2.46 2.25-2.75V8a1 1 0 0 1 1-1z" />
+                    </svg>
+
+                    <span class="menu-text hidden flex-1 ms-3 whitespace-nowrap">Billing & Invoicing</span>
+                </a>
+            </li>
 
             {{-- Setting --}}
             <li>
                 <a href="#"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                        class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-300 group-hover:text-green-600 dark:text-gray-400 dark:group-hover:text-white">
                         <path fill-rule="evenodd"
                             d="M11.078 2.25c-.917 0-1.699.663-1.85 1.567L9.05 4.889c-.02.12-.115.26-.297.348a7.493 7.493 0 0 0-.986.57c-.166.115-.334.126-.45.083L6.3 5.508a1.875 1.875 0 0 0-2.282.819l-.922 1.597a1.875 1.875 0 0 0 .432 2.385l.84.692c.095.078.17.229.154.43a7.598 7.598 0 0 0 0 1.139c.015.2-.059.352-.153.43l-.841.692a1.875 1.875 0 0 0-.432 2.385l.922 1.597a1.875 1.875 0 0 0 2.282.818l1.019-.382c.115-.043.283-.031.45.082.312.214.641.405.985.57.182.088.277.228.297.35l.178 1.071c.151.904.933 1.567 1.85 1.567h1.844c.916 0 1.699-.663 1.85-1.567l.178-1.072c.02-.12.114-.26.297-.349.344-.165.673-.356.985-.57.167-.114.335-.125.45-.082l1.02.382a1.875 1.875 0 0 0 2.28-.819l.923-1.597a1.875 1.875 0 0 0-.432-2.385l-.84-.692c-.095-.078-.17-.229-.154-.43a7.614 7.614 0 0 0 0-1.139c-.016-.2.059-.352.153-.43l.84-.692c.708-.582.891-1.59.433-2.385l-.922-1.597a1.875 1.875 0 0 0-2.282-.818l-1.02.382c-.114.043-.282.031-.449-.083a7.49 7.49 0 0 0-.985-.57c-.183-.087-.277-.227-.297-.348l-.179-1.072a1.875 1.875 0 0 0-1.85-1.567h-1.843ZM12 15.75a3.75 3.75 0 1 0 0-7.5 3.75 3.75 0 0 0 0 7.5Z"
                             clip-rule="evenodd" />
                     </svg>
+
 
                     <span class="menu-text hidden flex-1 ms-3 whitespace-nowrap">Setting</span>
                 </a>
--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,35 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+
+// Admin Panel Routes Group
+Route::prefix('admin')->middleware(['auth'])->group(function () {
+
+    // Employee Management
+    Route::get('/employmanagement', function () {
+        return 'admin/employmanagement';
+    })->name('admin.employmanagement.index');
+
+    // Attendance System
+    Route::get('/attendancesystem', function () {
+        return 'admin/attendancesystem';
+    })->name('admin.attendancesystem.index');
+
+    // Project Management
+    Route::get('/projectmanagement', function () {
+        return 'admin/projectmanagement';
+    })->name('admin.projectmanagement.index');
+
+    // Client Management
+    Route::get('/clientmanagement', function () {
+        return 'admin/clientmanagement';
+    })->name('admin.clientmanagement.index');
+
+    // Billing & Invoicing
+    Route::get('/billinginvoicing', function () {
+        return 'admin/billinginvoicing';
+    })->name('admin.billinginvoicing.index');
+});
+
