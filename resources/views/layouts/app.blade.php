f4cc6988df3e726f6bb7374b736a685ccc0bf7b8
Mohibbur Rahman <mohibbur1994@gmail.com>
2025-05-14 07:06:13 +0600

    web.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -122,6 +122,7 @@
     @stack('modals')
 
     @livewireScripts
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
 </body>
 
 
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\{
+    DepartmentController,
+    DesignationController,
+    EmployeeController,
+    AttendanceController,
+    AttendanceSettingController,
+    ClientController,
+    ClientContactController,
+    ClientNoteController,
+    ProjectController,
+    TaskController,
+    ProjectFileController,
+    ProjectNoteController,
+    InvoiceController,
+    PaymentController,
+};
+
+Route::resource('departments', DepartmentController::class);
+Route::resource('designations', DesignationController::class);
+Route::resource('employees', EmployeeController::class);
+Route::resource('attendances', AttendanceController::class);
+Route::resource('attendance-settings', AttendanceSettingController::class);
+Route::resource('clients', ClientController::class);
+Route::resource('client-contacts', ClientContactController::class);
+Route::resource('client-notes', ClientNoteController::class);
+Route::resource('projects', ProjectController::class);
+Route::resource('tasks', TaskController::class);
+Route::resource('project-files', ProjectFileController::class);
+Route::resource('project-notes', ProjectNoteController::class);
+Route::resource('invoices', InvoiceController::class);
+Route::resource('payments', PaymentController::class);
+
 
 Route::get('/', function () {
     return view('welcome');
