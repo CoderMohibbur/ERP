221cbbd95c0b9fa165b7486b3d21052fa3adbda5
Mohibbur Rahman <mohibbur1994@gmail.com>
2025-05-14 07:35:51 +0600

    department

--- app/Http/Requests/UpdateDepartmentRequest.php
+++ app/Http/Requests/UpdateDepartmentRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UpdateDepartmentRequest extends FormRequest
 {
@@ -11,18 +12,18 @@ class UpdateDepartmentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'name' => [
+                'required',
+                'string',
+                'max:100',
+                Rule::unique('departments', 'name')->ignore($this->department),
+            ],
         ];
     }
 }
